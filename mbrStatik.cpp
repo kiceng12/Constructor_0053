6a4d92a2f4c52c9956649cbf1915478af009ef7e
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-13 14:42:03 +0700

    firts commit

---
+++ mbrStatik.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+class mahasiswa
+{
+public:
+static int nim;
+int id;
+string nama;
+
+void setID();
+
+void printall();
+mahasiswa(string pnama):nama(pnama)
+{
+    setID();
+}
+};
+
+int mahasiswa::nim = 20;
+
+void mahasiswa::setID()
+{
+    
+}
