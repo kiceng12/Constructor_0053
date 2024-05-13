e9d0dc590aa5481636a4f9d90b65bd2827190487
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-13 14:47:20 +0700

    menulis void mahasiswa

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class mahasiswa
 {
 public:
-static int nim;
-int id;
-string nama;
+    static int nim;
+    int id;
+    string nama;
 
-void setID();
+    void setID();
 
-void printall();
-mahasiswa(string pnama):nama(pnama)
+    void printAll();
+    mahasiswa(string pnama):nama(pnama)
 {
     setID();
 }
@@ -22,5 +22,13 @@ int mahasiswa::nim = 20;
 
 void mahasiswa::setID()
 {
-    
+    id = nim++;
 }
+
+void mahasiswa::printAll()
+{
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
