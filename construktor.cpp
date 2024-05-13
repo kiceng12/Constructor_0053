58300302f9f9426ce73d013395b80b04cf86fdc6
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-13 16:58:29 +0700

    construktor

--- construktor.cpp
+++ construktor.cpp
@@ -8,14 +8,50 @@ class Mahasiswa
     int nim;
     string nama;
 
-    public:
     Mahasiswa()
     {
         nim = 0;
         nama = "";
     };
+
     Mahasiswa(int iNim)
     {
         nim = iNim;
-    }
+    };
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+    void cetak();
 };
+
+Mahasiswa::Mahasiswa(string iNama)
+{
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+    nim = iNim;
+    nama = iNama;
+};
+
+void Mahasiswa::cetak()
+{
+    cout << "NIM  = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(53);
+    Mahasiswa mhs3("Hendra");
+    Mahasiswa mhs4(53, "Hendra");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+    
+    return 0;  
+}
