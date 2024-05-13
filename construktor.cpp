f0b5341a87b6faae9885e3d2f7ae6de12e92b5be
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-13 15:29:09 +0700

    class mahasiswa

---
+++ construktor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+class Mahasiswa
+{
+    public:
+    int nim;
+    string nama;
+
+    public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    }
+};
